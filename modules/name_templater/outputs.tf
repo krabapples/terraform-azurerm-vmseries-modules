f65989e73ba6cea807e713fc58080d3479df2349
krabapples <koen.vanderpoorten@gmail.com>
2025-03-28 13:43:50 +0100

    Delete modules directory

--- modules/name_templater/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "template" {
-  description = "A template string that can be used with `format` function to generate a resource name."
-  value       = local.template_trimmed
-}
--- modules/natgw/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "natgw_pip" {
-  value = try(local.pip.ip_address, null)
-}
-
-output "natgw_pip_prefix" {
-  value = try(local.pip_prefix.ip_prefix, null)
-}
